016f5b358c47900bf819b597ca4ec24b81f98ff1
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-09 16:02:35 +0100

    implement container_wrapper

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,29 @@
 #pragma once
 
+#include <cstddef>
+#include <utility>
+
 template<typename T>
-class container_wrapper;
+class container_wrapper
+{
+public:
+  container_wrapper() = default;
+
+  container_wrapper(T t) :type{std::move(t)} 
+  {
+  }
+
+  container_wrapper(const container_wrapper&) = default;  
+  container_wrapper(container_wrapper&&) = default;
+  container_wrapper& operator=(const container_wrapper&) = default;
+  container_wrapper& operator=(container_wrapper&&) = default;
+
+  std::size_t size() const {
+    return type.size();
+  }
+
+private:
+  T type;
+
+};
 
