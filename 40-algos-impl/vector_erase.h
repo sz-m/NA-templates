395c20f81fff29636becec918ba957794a0fb0c3
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-10 15:59:53 +0100

    add 40- vector_erase

--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#include <algorithm>
+
 // define vector_erase here
+template<typename T>
+void vector_erase(T& vec, const typename T::value_type& value) 
+{
+  vec.erase(std::remove(vec.begin(), vec.end(), value), vec.end());
+}
 
