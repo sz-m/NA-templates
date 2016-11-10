c46698cc3c1e218d37b1737071041fd000305d3b
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-10 14:34:13 +0100

    add 40- find_if

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -3,6 +3,13 @@
 namespace training
 {
 template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred) 
+{
+  for(; begin != end ; ++begin) {
+    if(pred(*begin))
+      return begin;
+  }
+  return end;
+}
 }
 
