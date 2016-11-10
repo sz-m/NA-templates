598b543ac8c73aea6ed2fd00447294f0a93956c4
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-10 11:51:34 +0100

    add specializations

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -3,13 +3,15 @@
 #include <cstddef>
 #include <utility>
 
+#include <boost/optional.hpp>
+
 template<typename T>
 class container_wrapper
 {
 public:
   container_wrapper() = default;
 
-  container_wrapper(T t) :type{std::move(t)} 
+  container_wrapper(T arg) :_value{std::move(arg)} 
   {
   }
 
@@ -19,11 +21,33 @@ public:
   container_wrapper& operator=(container_wrapper&&) = default;
 
   std::size_t size() const {
-    return type.size();
+    return _value.size();
   }
 
 private:
-  T type;
+  T _value;
+};
 
+template<typename T>
+class container_wrapper<boost::optional<T>>
+{
+public:
+  container_wrapper() = default;
+
+  container_wrapper(boost::optional<T> arg) :_value{std::move(arg)} 
+  {
+  }
+
+  container_wrapper(const container_wrapper&) = default;  
+  container_wrapper(container_wrapper&&) = default;
+  container_wrapper& operator=(const container_wrapper&) = default;
+  container_wrapper& operator=(container_wrapper&&) = default;
+
+  std::size_t size() const {
+    return _value ? 1 : 0;
+  }
+
+private:
+  boost::optional<T> _value;
 };
 
