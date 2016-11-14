8a25a19f4e930f88147f31d1e37f16a250664c0d
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-14 11:22:54 +0100

    add 50- implementation

--- 50-sfinae/implementation.h
+++ 50-sfinae/implementation.h
@@ -1,8 +1,32 @@
 #pragma once
 
-template<typename T>
-struct has_type_typedef;
+#include <type_traits>
 
 template<typename T>
-struct has_member_variable;
+struct has_type_typedef
+{
+private:
+  template<typename U, typename = typename U::type>
+  static void test(int);
+
+  template<typename U>
+  static char test(long);
+
+public:
+  static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
+
+template<typename T>
+struct has_member_variable
+{
+private:
+  template<typename U>
+  static auto test(int) -> decltype(std::declval<U>().member, void());
+
+  template<typename U>
+  static char test(long);
+
+public:
+  static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
 
